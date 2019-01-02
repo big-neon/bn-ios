23bb7562bc0bd0c1ca905a037a6c49e4146d75ab
Gugulethu <gugulethu@Gugulethus-MacBook-Pro.local>
2019-01-02 23:23:18 +0200

    Completed the Profile Header View

--- BigNeonUI/Cells/Profile/ProfileHeaderView.swift
+++ BigNeonUI/Cells/Profile/ProfileHeaderView.swift
@@ -3,13 +3,6 @@ import UIKit
 
 final public class ProfileHeaderView: UIView {
     
-    public let profileHeaderView: UIView = {
-        let view = UIView()
-        view.backgroundColor = UIColor.green
-        view.translatesAutoresizingMaskIntoConstraints = false
-        return view
-    }()
-    
     public let profileImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.image = UIImage(named: "ic_emptyProfileImage")
@@ -65,28 +58,23 @@ final public class ProfileHeaderView: UIView {
         super.init(frame: frame)
         self.backgroundColor = UIColor.white
         self.layer.shadowColor = UIColor.black.cgColor
-        self.layer.shadowOffset = .zero
-        self.layer.shadowOpacity = 0.8
+        self.layer.shadowOffset = CGSize(width: 0.0, height: 1.0)
+        self.layer.shadowOpacity = 0.5
         self.layer.shadowRadius = 0.8
-        self.configureView()
         self.configureHeaderGradient()
+        self.configureView()
+        
     }
     
     private func configureView() {
-        self.addSubview(profileHeaderView)
         self.addSubview(profileImageView)
         self.addSubview(qrCodeImageView)
         self.addSubview(userNameLabel)
         self.addSubview(emailIconImageView)
         self.addSubview(userEmailLabel)
         
-        self.profileHeaderView.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
-        self.profileHeaderView.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
-        self.profileHeaderView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
-        self.profileHeaderView.heightAnchor.constraint(equalToConstant: 180).isActive = true
-        
         self.profileImageView.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 20).isActive = true
-        self.profileImageView.bottomAnchor.constraint(equalTo: profileHeaderView.bottomAnchor, constant: 29).isActive = true
+        self.profileImageView.bottomAnchor.constraint(equalTo: self.topAnchor, constant: 210).isActive = true
         self.profileImageView.heightAnchor.constraint(equalToConstant: 58).isActive = true
         self.profileImageView.widthAnchor.constraint(equalToConstant: 58).isActive = true
         
@@ -117,14 +105,14 @@ final public class ProfileHeaderView: UIView {
     
     private func configureHeaderGradient() {
         let gradient = CAGradientLayer()
-        gradient.frame = profileHeaderView.frame
-        gradient.startPoint = CGPoint(x: 0.5, y: 1.0)
-        gradient.endPoint = CGPoint(x: 0.5, y: 0.55)
-        let colors: [CGColor] = [UIColor(white: 1.0, alpha: 1.0).cgColor, UIColor(white: 1.0, alpha: 0.4).cgColor, UIColor(white: 1.0, alpha: 0.0).cgColor]
-        let location = [0.0, 0.6, 0.9]
+        gradient.frame = CGRect(x: 0.0, y: 0.0, width: self.frame.width, height: 180)
+        gradient.startPoint = CGPoint(x: 0.0, y: 1.0)
+        gradient.endPoint = CGPoint(x: 1.0, y: 0.0)
+        let colors: [CGColor] = [UIColor(red: 84.0/255.0, green: 145.0/255.0, blue: 204.0/255.0, alpha: 1.0).cgColor, UIColor(red: 222.0/255.0, green: 65.0/255.0, blue: 152.0/255.0, alpha: 1.0).cgColor]
+        let location = [0.0, 1.0]
         gradient.colors = colors
         gradient.isOpaque = true
         gradient.locations = location as [NSNumber]
-        profileHeaderView.layer.addSublayer(gradient)
+        self.layer.addSublayer(gradient)
     }
 }
