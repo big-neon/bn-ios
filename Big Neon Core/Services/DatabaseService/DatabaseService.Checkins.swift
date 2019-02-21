f1a04fcd286cd23a365ab919cf7e3c279a5d0526
Gugulethu <mhlangagc@gmail.com>
2019-02-21 10:45:10 +0200

    Removed an unwanted comment

--- Big Neon Core/Services/DatabaseService/DatabaseService.Checkins.swift
+++ Big Neon Core/Services/DatabaseService/DatabaseService.Checkins.swift
@@ -42,38 +42,4 @@ extension DatabaseService {
                 }
         }
     }
-    
-//    public func fetchCh(withID eventID: String, completion: @escaping (Error?, EventDetail?) -> Void) {
-//
-//        let APIURL = APIService.getEvents + "/" + eventID
-//        AF.request(APIURL,
-//                   method: HTTPMethod.get,
-//                   parameters: nil,
-//                   encoding: JSONEncoding.default,
-//                   headers: [:])
-//            .validate(statusCode: 200..<300)
-//            .response { (response) in
-//
-//                guard response.result.isSuccess else {
-//                    completion(response.result.error, nil)
-//                    return
-//                }
-//
-//                guard let data = response.result.value else {
-//                    completion(nil, nil)
-//                    return
-//                }
-//
-//                do {
-//                    let decoder = JSONDecoder()
-//                    let event = try decoder.decode(EventDetail.self, from: data!)
-//                    completion(nil, event)
-//                    return
-//                } catch let error as NSError {
-//                    print(error)
-//                    completion(error, nil)
-//                }
-//        }
-//    }
-    
 }
